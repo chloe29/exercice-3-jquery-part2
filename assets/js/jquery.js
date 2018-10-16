4e20d06b7c947be72a917317a0a03b08fd82d0fa
lavisse <chloe.lavisse80@gmail.com>
2018-10-16 12:02:29 +0200

    envoi des fichiers

---
+++ assets/js/jquery.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('#show').click(function(){
+    $('#text').show();
+  });
+  // lorsque je click sur l'ancre Afficher, le texte apparait
+  $('#hide').click(function(){
+    $('#text').hide();
+  });
+  // lorsque je click sur l'ancre Masquer, le texte disparait
+});
